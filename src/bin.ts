3d5b5d22eae1ffcdb3ce13d927f404638cfe4c71
Ray Deck <ray@raydeck.com>
2020-08-15 21:07:49 -0500

    feat: first commit

--- src/bin.ts
+++ src/bin.ts
@@ -1,4 +1,23 @@
 #!/usr/bin/env node
 import commander from "commander";
+import { getConfig, writeConfig } from "./";
+
+commander.option(
+  "-s --stage [stage]",
+  "Stage for the stack we are examining (default from monorepo root)"
+);
+commander.option(
+  "-r --region [region]",
+  "Region for the stack (default us-east-1)"
+);
 commander.parse(process.argv);
 export { commander };
+const { region, stage, appSyncPath } = commander;
+if (!commander.isDocumenting)
+  (async () => {
+    try {
+      writeConfig(await getConfig({ region, stage, appsyncPath: appSyncPath }));
+    } catch (e) {
+      console.error("Could not generate appsync config:", e);
+    }
+  })();
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { join } from "path";
+import { existsSync, mkdirSync, writeFileSync, readFileSync } from "fs";
+import { getAppSync, getResources } from "@raydeck/serverless-resources";
+import { findStage } from "@raydeck/serverless-stage";
+import { getServerlessConfig } from "@raydeck/serverless-base";
+import type { PromiseValue } from "type-fest";
+export async function getConfig({
+  region = "us-east-1",
+  stage = findStage() ?? "dev",
+  path = process.cwd(),
+  appsyncPath,
+}: {
+  region?: string;
+  stage?: string;
+  path?: string;
+  appsyncPath?: string;
+}) {
+  if (!appsyncPath) {
+    const { appsync } = getServerlessConfig(path);
+    appsyncPath = appsync;
+  }
+  if (!appsyncPath)
+    throw "Cannot proceed without a valid path to appsync in package.json or passed in arguments ";
+  const r = await getResources({ region, stage, path: appsyncPath });
+  const api = await getAppSync(r, { region, stage });
+  if (!api) throw new Error("Could not get graphql api");
+  return {
+    graphqlEndpoint: api.uris!["GRAPHQL"],
+    region,
+    authenticationType: api.authenticationType,
+  };
+}
+export function writeConfig(
+  config: PromiseValue<ReturnType<typeof getConfig>>,
+  path = join(process.cwd(), "config")
+) {
+  if (!existsSync(path)) mkdirSync(path);
+  const fullPath = join(path, "appsync.json");
+  writeFileSync(fullPath, JSON.stringify(config, null, 2));
+}
